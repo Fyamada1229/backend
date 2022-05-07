51c5c25b127201862c068a4f23b7995662d2110c
Fyamada1229 <yamadafutosi1229@gmail.com>
2022-05-07 10:44:53 +0900

    add: seeder追加

--- app/app/Http/Controllers/API/UserController.php
+++ app/app/Http/Controllers/API/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -25,7 +26,11 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = User::create($request->all());
+        return response()->json(
+            $user,
+            201
+        );
     }
 
     /**
